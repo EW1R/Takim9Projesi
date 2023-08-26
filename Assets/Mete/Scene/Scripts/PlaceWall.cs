8acabd62f4ee5e4054b45db7227d4e276d97f745
Barış Mete <brs.mete7@gmail.com>
2023-08-26 18:34:51 +0300

    added x

--- Assets/Mete/Scene/Scripts/PlaceWall.cs
+++ Assets/Mete/Scene/Scripts/PlaceWall.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlaceWall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 place;
 
-    // Update is called once per frame
+    private RaycastHit _hit;
+
+    public GameObject objectPlace, tempObject;
+
+    public GameObject iceWall, tempIceWall;
+
+    public bool placeNow;
+    public bool placeActive;
+    public bool placeDeactive;
+
     void Update()
     {
+        if (placeNow == true)
+            SendRay();
+
+        if (placeActive == true)
+            objectPlace = iceWall;
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
         
+        }
+
     }
+
+    void SendRay()
+    {
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out _hit))
+        {
+            place = new Vector3(_hit.point.x,_hit.point.y,_hit.point.z);
+
+            if(placeDeactive == false)
+            {
+                Instantiate(tempIceWall,place,Quaternion.identity);
+                tempObject = GameObject.Find("IceWall");
+                placeDeactive= true;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(objectPlace,place,Quaternion.identity);
+                placeNow = false;
+                placeActive = false;
+                
+
+
+            }
+        }
+    }
+
+
 }
