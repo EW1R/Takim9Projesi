e375750b976b2e0f880faf1ef7b33131d1597a4a
Yiğit Efe Sert <yigitefesert@gmail.com>
2023-08-26 18:16:26 +0300

    masonal
    31

--- Assets/Yes/scripts/bloktasi.cs
+++ Assets/Yes/scripts/bloktasi.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class bloktasi : MonoBehaviour
 {
     public GameObject blockPrefab;
-    private GameObject previewBlock;
+    public GameObject previewBlock;
     private bool isDragging = false;
 
     private void Update()
@@ -13,7 +14,7 @@ public class bloktasi : MonoBehaviour
         if (Input.GetMouseButtonDown(1)) // Sað týklandýðýnda
         {
             isDragging = true;
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+           Ray ray = 
             mousePosition.z = 0f;
             previewBlock = Instantiate(blockPrefab, mousePosition, Quaternion.identity);
         }
@@ -28,6 +29,7 @@ public class bloktasi : MonoBehaviour
         if (Input.GetMouseButtonUp(1)) // Sað týklamayý býraktýðýnýzda
         {
             isDragging = false;
+
             Destroy(previewBlock);
             Vector3 finalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             finalPosition.z = 0f;
