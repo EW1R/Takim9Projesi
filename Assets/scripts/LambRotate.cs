e06853ce8f8c97a2ab09b31d4d3eba7c6d5cbf7a
Yiğit Efe Sert <yigitefesert@gmail.com>
2023-08-26 13:38:36 +0300

    Ana Menü Taslak
    Menü ekranı taslak kaydetmelik

---
+++ Assets/scripts/LambRotate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lambrotate : MonoBehaviour
+{
+
+    public float maxRotationAmount = 90.0f; // Maksimum dönüþ açýsý
+    public float shakeSpeed = 1.3f; // Sallanma hýzý
+
+    private Vector3 initialRotation;
+    private float rotationTimer = 0.0f;
+
+    private void Start()
+    {
+        initialRotation = transform.rotation.eulerAngles;
+    }
+
+    private void Update()
+    {
+        // Zamanlayýcýyý artýrarak sallanma efektini yarat
+        rotationTimer += Time.deltaTime * shakeSpeed;
+
+        // Maksimum dönüþ açýsýný hesaplayarak, sinüs fonksiyonunu kullanarak sallanma efektini oluþtur
+        float currentRotationAmount = Mathf.Sin(rotationTimer) * maxRotationAmount;
+
+        // Eski rotasyonu koruyarak yalnýzca nesnenin yatay dönüþünü deðiþtir
+        Quaternion newRotation = Quaternion.Euler(initialRotation.x, initialRotation.y + currentRotationAmount, initialRotation.z);
+
+        // Nesneyi yeni rotasyonla güncelle
+        transform.rotation = newRotation;
+    }
+}
