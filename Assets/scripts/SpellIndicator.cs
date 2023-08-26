d85c0130aa55d218907e952e1c650ad67c8ef708
Oğulcan <ogulcankarakas08v2@gmail.com>
2023-08-26 18:24:48 +0300

    asads

---
+++ Assets/scripts/SpellIndicator.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SpellIndicator : MonoBehaviour
+{
+    public GameObject indicatorPrefab; // Gösterge için kullanýlacak prefab
+    private GameObject indicator; // Oluþturulan gösterge
+
+    void Update()
+    {
+        // Büyü yeteneði kullanýlacaðýnda (örneðin, fare sol týklamada)
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Ýndikatörü oluþtur
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10; // Uzaklýðý ayarlayýn
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            indicator = Instantiate(indicatorPrefab, worldPosition, Quaternion.identity);
+        }
+
+        // Büyü yeteneði kullanýmýný bitirdiðinizde
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Ýndikatörü yok et
+            if (indicator != null)
+            {
+                Destroy(indicator);
+            }
+        }
+    }
+}
+
