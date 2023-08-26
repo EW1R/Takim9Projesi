f42ffb34e12b3a5a8314cb5b585156da9c354352
Oğulcan <ogulcankarakas08v2@gmail.com>
2023-08-26 19:35:33 +0300

    calısan

--- Assets/ogi/SpellIndıcator.cs
+++ Assets/ogi/SpellIndıcator.cs
@@ -6,20 +6,22 @@ using UnityEngine;
 
 public class SpellIndicator : MonoBehaviour
 {
-    public GameObject indicatorPrefab; // Gösterge için kullanılacak prefab
+    public GameObject indicatorPrefab, tempObject; // Gösterge için kullanılacak prefab
     private GameObject obje; // Oluşturulan gösterge
+    private bool isIndicating = false;
+    private GameObject temp;
+    private bool isSpawned = false;
+    private void Start()
+    {
 
+    }
     void Update()
     {
         // Büyü yeteneği kullanılacağında (örneğin, fare sol tıklamada)
         if (Input.GetMouseButtonDown(1))
         {
-            // İndikatörü oluştur
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = 10; // Uzaklığı ayarlayın
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            obje = Instantiate(indicatorPrefab, worldPosition, Quaternion.identity);
+            isIndicating = true;
+            isSpawned = false;
         }
 
         // Büyü yeteneği kullanımını bitirdiğinizde
@@ -28,9 +30,50 @@ public class SpellIndicator : MonoBehaviour
             // İndikatörü yok et
             if (obje != null)
             {
+                isIndicating = false;
                 Destroy(obje);
             }
         }
+        if (Input.GetMouseButtonUp(1)) 
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10; // Uzaklığı ayarlayın
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            Instantiate(tempObject, worldPosition, Quaternion.identity);
+        }
+        if (!isSpawned)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10; // Uzaklığı ayarlayın
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            Create(worldPosition);
+            isSpawned = !isSpawned;
+        }
+        if (isIndicating && isSpawned)
+        {
+            Indıcator();
+        }
+    }
+
+    private void Indıcator()
+    {
+        // İndikatörü oluştur
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10; // Uzaklığı ayarlayın
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        print("1" + mousePosition);
+        print("2" + Input.mousePosition);
+        print("3" + worldPosition);
+        obje = temp;
+        print("OBJE" + obje.transform.position);
+        obje.transform.position = worldPosition;
+    }
+
+    private GameObject Create(Vector3 worldPos)
+    {
+        temp = Instantiate(indicatorPrefab, worldPos, Quaternion.identity);
+        return temp;
     }
 }
 
