20fd5916ada4eb02e482dd3f0ad1b77ba3f50312
Barış Mete <brs.mete7@gmail.com>
2023-08-27 16:16:29 +0300

    mete scene

--- Assets/Dincer/Script/Health.cs
+++ Assets/Dincer/Script/Health.cs
@@ -24,7 +24,6 @@ public class Health : MonoBehaviour
     void Update()
     {
         currentHealthBar.fillAmount = currentHealth / healthAmount;
-        totalHealthBar.fillAmount = currentHealth / healthAmount;
     }
 
 
