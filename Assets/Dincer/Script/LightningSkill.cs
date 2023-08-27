878aa9341445faa5cbe78ed94e1319041c40c74d
DinçerÖğren <emre.ogren@ogr.sakarya.edu.tr>
2023-08-27 22:08:55 +0300

    Fireball

--- Assets/Dincer/Script/LightningSkill.cs
+++ Assets/Dincer/Script/LightningSkill.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [CreateAssetMenu(fileName ="Lightning",menuName = "Ability/Spell2")]
 public class LightningSkill : Spell2
 {
+
     public override void Activate()
     {
         base.Activate();
-        Debug.Log("YILDIRIM");
+
+        
+
+        var collisions= Physics.OverlapSphere(castPos.position, raycastRadius);
+
+        foreach (var item in collisions)
+        {
+            if (item.TryGetComponent<Health>(out Health enemy))
+            {
+                enemy.TakeDamage(damage);
+                enemy.GetComponent<NavMeshAgent>().SetDestination(enemy.transform.position);
+
+            }
+
+        }
 
     }
+    
 
 }
